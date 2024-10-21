cda12f0349f5023756620d006331b2c41dee3888
UsmanKodvavi <u3166004@gmail.com>
2024-10-21 20:03:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,178 @@
+// alert("Error ! Please enter a valid password" )
+
+// alert("welcome to our website" )
+
+// alert("Welcome to JS Land...   Happy coding!  " )
+
+// alert("   Welcome to JS Land....  " )
+
+// alert("   Happy coding....  " )
+
+// var Javascript ("Hello... I can run JS thorgh Web broser console")
+
+// console.log('Javascript')
+
+
+// chapter no 2
+
+
+
+// var Username = "hi..."
+
+// var Myname = "Hi... class"
+
+// alert("Hello World ('Happy coding') ")
+
+// var name = "Jhon Doee "
+
+// var age = 16
+
+// var ability = "certified mobile application developer"
+
+// alert("Jhon Doee")
+
+// alert(16)
+
+// alert("certified mobile application developer")
+
+// console.log(name)
+
+// console.log(age)
+
+// console.log(ability)
+
+// alert("I am trying to learn javascript from A Smarter Way to Learn Javascript")
+
+// document.write("Yah! I Can Write HTMl through Javasrcipt")
+
+
+// chapter No 3
+
+
+// var age = 17
+
+// alert("My AGE is 17 Years")
+
+// var birthyear = "30 dec 2007"
+
+// console.log(birthyear , 'birthyear')
+
+// document.write("My Birth Year Is 2007 <br/> "  )
+
+// document.write("Data Type of My declared Variable is Number")
+
+// var visitorname = "Jhon Doee"
+
+// var product = "Pent-shirt"
+
+// var Quantity = 5
+
+// document.write("Jhon Doee ordered 5-T shirt on ABC clothing store")
+
+// Assigment No 6
+
+
+// var name = prompt("Enter Your Name")
+
+
+// alert("Welcome Sir")
+
+
+
+
+
+
+// chapetr No 7
+
+// var cities = [karachi , lahore , multan]
+
+// var city = prompt("Enter Your City")
+
+// if (city == karachi) {
+// "welcome to the city of lights "
+// }
+//  else if (city !== karachi) {
+// "Welcome Sir/Maddam"
+// }
+
+// var gender = prompt("Enter Your gender")
+
+// if (gender == male) {
+// "welcome sir "
+// };
+// if else (gender == female) {
+// "Welcome Maddam"
+// };
+
+// var num1 = +prompt("Enter Number 1")
+// var num2 = +prompt("Enter Number 2")
+// console.log(num1 + num2 , "Add")
+// console.log(num1 - num2 , "Sub")
+// console.log(num1 * num2 , "Mul")
+// console.log(num1 / num2 , "Div")
+
+// chapter No 8
+
+
+
+// var userPassword = prompt("Please enter your password:");
+
+//  var validate = 'Validate the two passwords'
+// if (userPassword === null || userPassword.trim() === "") {
+//     // i. Check if user has entered a password
+//     alert("Please enter your password");
+// } else if (userPassword === correctPassword) {
+//     // ii. Check if both passwords are the same
+//     alert("Correct! The password you entered matches the original password.");
+// } else {
+//     alert("Incorrect password.");
+// }
+
+ 
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";}
+
+
+// console.log(greeting);
+
+
+// var userTime = prompt("Enter time");
+// if (userTime >= 0 && userTime <= 6) {
+//   alert("Good night!");
+// } else if (userTime >= 7 && userTime <= 12) {
+//   alert("Good Morning!");
+// } else if (userTime >= 13 && userTime <= 18) {
+//   alert("Good afternoon!");
+// } else if (userTime >= 19 && userTime <= 23) {
+//   alert("Good Evening!");
+// } else {
+//   alert("Invalid Time");
+// }
+
+
+// Assigment No 9
+
+
+// var string = ['karachi' , 'lahore' , 'multan' , 'india'];
+
+// var numbers = [1 , 2 , 3 ,4 , 5];
+
+// var boolean = [true , false ,  false , true ];
+
+// var mix = [ "karachi" , 22 , true , "lahore" , false , 33]; 
+
+
+// var ability = ['SSC' , 'HSC' , 'BCS' , 'CS' , 'BCOM' ]
+
+
+
+// var score = ["Score of Michal is 220"  ,  "Score of Jhon is 320 " , "Score of latin is 420"];
+
+// var percentage = ["Percentage is 64%" , "Percentage is 70%" , "Percentage is 75%"];
+
+// document.write(score , percentage);
+
